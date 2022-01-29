8d9376c8bd6b4bdde511fd1214adb75b59b356d9
Christopher Mitchell <cmitchell@crishell.co>
2022-01-29 11:36:04 -0500

    fix(card): hide header and footer if no slot content

--- src/components/Card/Card.vue
+++ src/components/Card/Card.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="bg-white overflow-hidden shadow rounded-lg divide-y divide-gray-200">
-    <div class="px-4 py-5 text-lg leading-6 font-medium text-gray-900"><slot name="header" /></div>
+    <div v-if="$slots.header" class="px-4 py-5 text-lg leading-6 font-medium text-gray-900"><slot name="header" /></div>
     <div class="px-4 py-5">
       <slot />
     </div>
-    <div class="px-4 py-4 bg-gray-50"><slot name="footer" /></div>
+    <div v-if="$slots.footer" class="px-4 py-4 bg-gray-50"><slot name="footer" /></div>
   </div>
 </template>
